191d367b94901354d8e17d0d13f1430a2057b64f
FLP013 <felliperibeiro9@gmail.com>
2024-09-07 00:16:41 -0300

    programa finalizado

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+ 
+Console.WriteLine("de tantos instantes");
+Console.WriteLine("para mim lembrança");
+Console.WriteLine("as flores de cerejeira.");
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("(Matsuo Bashô)");
+Console.ResetColor();
